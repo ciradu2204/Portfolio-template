bb48a9639de742290bed13f1c186dfb52554102b
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-02-19 20:51:34 +0200

    worked on events

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -4,13 +4,33 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-import howtothrive from "../img/events/howtothriveintech.png";
-import myhumblejourney from "../img/events/howtothriveintech.png";
+import howtothrive from "../../img/events/howtothriveintech.png";
+import myhumblejourney from "../../img/events/myhumbleJourney.png";
+import contributhon from "../../img/events/contributonwrapup.jpeg";
 
-
+import "./index.css"
+import { GrNext, GrPrevious } from 'react-icons/gr';
 const Event = () => {
+    const initialEndIndex = () => {
+        if (window.innerWidth < 480) {
+          return 0;
+        } else if (window.innerWidth < 800) {
+          return 1;
+        } else {
+          return 2;
+        }
+      };
+    const [startIndex, setStartIndex] = React.useState(0);
+    const [endIndex, setEndIndex] = React.useState(initialEndIndex());
+
 
     const events = [
+        {title: "She Code Africa Contributhon First Cohort Graduation",
+        img: contributhon,
+        description: "During the contributhon first cohort graduation, I gave a speech representing all the women who were part of the first cohort. The speech aimed to thank the organisers and open source organisations which were part of the cohort",
+        attendees: 40
+        }
+        ,
         {
             title: "How to Thrive in Tech as a Woman",
             img: howtothrive,
@@ -21,42 +41,62 @@ const Event = () => {
             title: "My humble Journey to Google",
             img: myhumblejourney,
             description: "Eunice Allela and Ebenezer Graham, recent graduates, and full-time Googlers shared their experience in college and advice on what students can do to get a good internship or job after college.",
-            attendes: 23
+            attendees: 23
+        },
+        {
+           title: "GDSC ALC Info Session",
+           img: "",
+           description: "An info session to introduce students to what is GDSC(Google Developer Student Club) and the different opportunities we would be sharing throughout the year 2021.",
+           attendees: 25,
         },
     ]
 
+    const handlePrev = () => {
+        if(startIndex > 0){
+            setStartIndex((prev) => prev -1);
+            setEndIndex((prev) => prev -1);
+        }
+    }
+
+    const handleNext = () => {
+        if(endIndex < events.length -1){
+            setStartIndex((prev) => prev +1);
+            setEndIndex((prev) => prev +1);
+        }
+    }
+
     return (
-        <>
-      {events.map((event) => (
+      <div className='eventsContainer'>
+        {startIndex > 0 &&<GrPrevious onClick={handlePrev} className="icon"/>}
+      {events.filter((element, index) => index >= startIndex && index <=endIndex).map((event) => (
         <EventTemplate event={event} />
       ))}
-       </>
+         {endIndex < events.length -1 && <GrNext  onClick={handleNext} className="icon"/>}
+       </div>
     )
 }
 export default Event;
 
 export function EventTemplate({event}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+    <Card className="card">
         <CardMedia
           component="img"
-          height="140"
-          image={event.image}
+          height="150"
+          image={event.img}
           alt={event.title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent className='cardContent'>
+          <Typography gutterBottom variant="p" component="div">
             {event.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" >
             {event.description}
           </Typography>
         </CardContent>
-      </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          {event.attendes}
+        <Button size="small" color="inherit">
+          {event.attendees} Attendees
         </Button>
       </CardActions>
     </Card>
--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,10 +1,14 @@
 import {EventsHeader, EventsIntro} from "./EventsElement"
+import EventsHome from "../../components/Events"
+import Footer from "../../components/SharedComponents/Footer"
 const Events = () => {
 
     return (
         <>
         <EventsHeader>EVENTS</EventsHeader>
-        <EventsIntro>The different events I organised and hosted during university and after</EventsIntro>
+        <EventsIntro>The different events I organised, hosted or invited to speak starting from university to now.</EventsIntro>
+        <EventsHome/>
+        <Footer/>
         </>
     )
 }
