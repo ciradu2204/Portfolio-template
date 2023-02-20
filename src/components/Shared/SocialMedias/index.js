43b55ba2e38d22ba415147a69053ca2fa1210793
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-02-20 17:01:24 +0200

    updated social media links

--- src/components/Shared/SocialMedias/index.js
+++ src/components/Shared/SocialMedias/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Mail, Twitter, Wrapper, Link, LinkedIn} from './socialMediaElements'
+import {Mail, Twitter, Wrapper, Link, LinkedIn, Github} from './socialMediaElements'
 
 const SocialMedia = ({direction, color}) =>{
    let channels = {
         Twitter: 'https://twitter.com/IraduCynthia',
         LinkedIn: 'https://www.linkedin.com/in/ciradu2204/',
         Email: 'ciradukunda2204@gmail.com',
+        Github: 'https://github.com/ciradu2204',
     };
 
     return (
@@ -18,6 +19,9 @@ const SocialMedia = ({direction, color}) =>{
           </Link>
           <Link target="_blank" href={channels.LinkedIn}>
           <LinkedIn iconcolor={color} />
+          </Link>
+          <Link target="_blank" href={channels.Github}>
+          <Github iconcolor={color} />
           </Link>
           </Wrapper>
       )
--- src/components/Shared/SocialMedias/socialMediaElements.js
+++ src/components/Shared/SocialMedias/socialMediaElements.js
@@ -1,6 +1,8 @@
 import {AiFillTwitterCircle} from "react-icons/ai";
 import {AiOutlineMail} from  'react-icons/ai';
 import { GrLinkedin } from "react-icons/gr";
+import { GrGithub } from "react-icons/gr";
+
 import styled from "styled-components"
 
 
@@ -14,16 +16,20 @@ justify-content: center;
 
 export const Mail = styled(AiOutlineMail)`
 color: ${props => props.iconcolor || "#C0A062"};
-font-size: 1rem;
+font-size: 1.5rem;
 `
 
 export const LinkedIn = styled(GrLinkedin)`
 color: ${props => props.iconcolor || "#C0A062"};
-font-size: 1rem;
+font-size: 1.5rem;
 `
 export const Twitter = styled(AiFillTwitterCircle)`
 color: ${props => props.iconcolor || "#C0A062"};
-font-size: 1rem;
+font-size: 1.5rem;
+`
+export const Github = styled(GrGithub)`
+color: ${props => props.iconcolor || "#C0A062"};
+font-size: 1.5rem;
 `
 export const Link = styled.a`
  text-decoration: none;
