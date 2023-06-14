099a36bf2746627ae1d23f74c12a39cdfd9b37ee
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-06-14 14:37:46 +0200

    updated resume

--- client/src/pages/CV/index.js
+++ client/src/pages/CV/index.js
@@ -2,14 +2,14 @@ import { GrDownload } from 'react-icons/gr';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import Footer from '../../components/Shared/Footer';
 import "./index.css"
-import resume from "../../img/MyResume.pdf"
+import resume from "../../img/Cynthia-Iradukunda-Resume.pdf"
 const CV = () => {
 
     return (
     <>
     <div className='resumeContainer'>
     <a className='downloadResume' href={resume} download>Download Resume <GrDownload/></a>
-    <Document file="MyResume.pdf" >
+    <Document file="Cynthia-Iradukunda-Resume.pdf" >
         <Page pageNumber={1} className="page"/>
     </Document>
    </div>
